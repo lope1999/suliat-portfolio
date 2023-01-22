bff5e92ecbe4e9769f228cd26e39af35c71263d8
lope1999 <corps.technical2.union@gmail.com>
2023-01-22 11:23:29 +0100

    Initial Push - Home Page

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,135 @@
+import './index.scss'
+import { useState } from 'react'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faWhatsapp,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <Link 
+        className="logo"
+        to="/"
+        onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="suliat" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about-suliat"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact-suliat"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ee82c7"
+          size="3x"
+          className='close-icon' />
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/suliat-alaga-57b023149/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/lope1999"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://wa.me/message/FQRO5SITKCHYG1"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faWhatsapp}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/AlagaSuliat" 
+            rel="noreferrer"
+           target="_blank"
+           >
+            <FontAwesomeIcon
+              icon={faTwitter}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ee82c7"
+          size="3x"
+          className='hamburger-icon' />
+    </div>
+  )
+}
+
+export default Sidebar
